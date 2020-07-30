31cdd64bab2c582b69ac79e0327435bba4819846
TeresaYang <s1022159@gm.pu.edu.tw>
2020-07-30 22:07:37 +0800

    增加iphone網頁的價格選擇

---
+++ webs/Project_JS_iPhone/assets/script.js
@@ -0,0 +1,29 @@
+function toWhite() {
+    var changepic = document.querySelector('#iphone-image');
+    changepic.src = "image/iphone11-white-select-2019.png";
+  }
+
+  function toBlack() {
+    var changepic = document.querySelector('#iphone-image');
+    changepic.src = "image/iphone11-black-select-2019.png";
+  }
+
+  function toGreen() {
+    var changepic = document.querySelector('#iphone-image');
+    changepic.src = "image/iphone11-green-select-2019.png";
+  }
+
+  function toYellow() {
+    var changepic = document.querySelector('#iphone-image');
+    changepic.src = "image/iphone11-yellow-select-2019.png";
+  }
+
+  function toPurple() {
+    var changepic = document.querySelector('#iphone-image');
+    changepic.src = "image/iphone11-purple-select-2019.png";
+  }
+
+  function toRed() {
+    var changepic = document.querySelector('#iphone-image');
+    changepic.src = "image/iphone11-red-select-2019.png";
+  }
