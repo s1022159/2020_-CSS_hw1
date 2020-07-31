73eede8646becdad605f17914189ad752e6e17c8
TeresaYang <s1022159@gm.pu.edu.tw>
2020-07-31 17:57:50 +0800

    iphone網頁修改，將原本是一直是重複的顏色程式碼寫成函式

--- webs/Project_JS_iPhone/assets/script.js
+++ webs/Project_JS_iPhone/assets/script.js
@@ -1,29 +1,19 @@
-function toWhite() {
-    var changepic = document.querySelector('#iphone-image');
+function setColor(color) {
+  var changepic = document.querySelector('#iphone-image');
+  if (color == 'white') {
     changepic.src = "image/iphone11-white-select-2019.png";
   }
-
-  function toBlack() {
-    var changepic = document.querySelector('#iphone-image');
+  if (color == 'black') {
     changepic.src = "image/iphone11-black-select-2019.png";
   }
-
-  function toGreen() {
-    var changepic = document.querySelector('#iphone-image');
+  if (color == 'green') {
     changepic.src = "image/iphone11-green-select-2019.png";
   }
-
-  function toYellow() {
-    var changepic = document.querySelector('#iphone-image');
-    changepic.src = "image/iphone11-yellow-select-2019.png";
-  }
-
-  function toPurple() {
-    var changepic = document.querySelector('#iphone-image');
+  if (color == 'purple') {
     changepic.src = "image/iphone11-purple-select-2019.png";
   }
-
-  function toRed() {
-    var changepic = document.querySelector('#iphone-image');
+  if (color == 'red') {
     changepic.src = "image/iphone11-red-select-2019.png";
   }
+}
+
